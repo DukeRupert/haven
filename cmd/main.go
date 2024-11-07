750298378ed47eae4e21dfa8aeadbe078d3dd7b9
dukerupert <logan@fireflysoftware.dev>
2024-11-06 20:32:16 -0700

    Proof of concept

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/DukeRupert/haven/handler"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
+	userHandler := handler.UserHandler{}
+	e.GET("/user", userHandler.HandleUserShow)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World! Welcome to Haven.")
 	})
