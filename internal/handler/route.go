381af2abf9813660e86e8108330c4568b593d9da
dukerupert <logan@fireflysoftware.dev>
2025-01-02 14:06:36 -0700

    bugfix: updated create user calls

--- internal/handler/route.go
+++ internal/handler/route.go
@@ -10,11 +10,11 @@ import (
 )
 
 /*
-Public: 			/
-Authenticated:		/app/
-Super:				/app/facilities/
-Facility:			/app/facility_code/
-User:				/app/facility_code/user_initials/
+Public:         /
+Authenticated:  /app/
+Super:          /app/facilities/
+Facility:       /app/facility_code/
+User:           /app/facility_code/user_initials/
 */
 
 const (
@@ -73,74 +73,74 @@ func setupPublicRoutes(e *echo.Echo, h *Handler) {
 }
 
 func setupAppRoutes(e *echo.Echo, h *Handler, m *middleware.Middleware) {
-   // Base app group with auth
-   app := e.Group("/app", m.Auth(), m.RouteContext())
-   // Complete path: /app/calendar
-   app.GET("/calendar", h.HandleCalendar)
-   // Complete path: /app/profile  
-   app.GET("/profile", h.HandleGetUser)
+	// Base app group with auth
+	app := e.Group("/app", m.Auth(), m.RouteContext())
+	// Complete path: /app/calendar
+	app.GET("/calendar", h.HandleCalendar)
+	// Complete path: /app/profile
+	app.GET("/profile", h.HandleGetUser)
 
-   // Facility management (require super role)
-   facilities := app.Group("/facilities", m.RequireRole(types.UserRoleSuper))
-   {
-       // Complete path: /app/facilities
-       facilities.GET("", h.HandleGetFacilities)
-       facilities.POST("", h.HandleCreateFacility)
-       // Complete path: /app/facilities/create
-       facilities.GET("/create", h.GetCreateFacilityForm)
-       // Complete path: /app/facilities/edit
-       facilities.GET("/edit", h.GetUpdateFacilityForm)
-       // Complete path: /app/facilities/:facility_id
-       facilities.PUT(PathFacilityID, h.HandleUpdateFacility)
-       facilities.DELETE(PathFacilityID, h.HandleDeleteFacility)
-   }
+	// Facility management (require super role)
+	facilities := app.Group("/facilities", m.RequireRole(types.UserRoleSuper))
+	{
+		// Complete path: /app/facilities
+		facilities.GET("", h.HandleGetFacilities)
+		facilities.POST("", h.HandleCreateFacility)
+		// Complete path: /app/facilities/create
+		facilities.GET("/create", h.GetCreateFacilityForm)
+		// Complete path: /app/facilities/edit
+		facilities.GET("/edit", h.GetUpdateFacilityForm)
+		// Complete path: /app/facilities/:facility_id
+		facilities.PUT(PathFacilityID, h.HandleUpdateFacility)
+		facilities.DELETE(PathFacilityID, h.HandleDeleteFacility)
+	}
 
-   // Facility routes (require facility access)
-   facility := app.Group(PathFacilityCode, m.RequireFacilityAccess())
-   {
-       // Complete path: /app/:facility_code/calendar
-       facility.GET("/calendar", h.HandleCalendar)
-       // Complete path: /app/:facility_code/publish
-       facility.PUT("/publish", h.HandleUpdatePublishedThrough)
-   }
+	// Facility routes (require facility access)
+	facility := app.Group(PathFacilityCode, m.RequireFacilityAccess())
+	{
+		// Complete path: /app/:facility_code/calendar
+		facility.GET("/calendar", h.HandleCalendar)
+		// Complete path: /app/:facility_code/publish
+		facility.PUT("/publish", h.HandleUpdatePublishedThrough)
+	}
 
-   // User management routes (requires admin role)
-   users := facility.Group("/users", m.RequireRole(types.UserRoleAdmin))
-   {
-       // Complete path: /app/:facility_code/users
-       users.GET("", h.HandleUsers)
-       users.POST("", h.HandleCreateUser)
-       // Complete path: /app/:facility_code/users/create
-       users.GET("/create", h.GetCreateUserForm)
-   }
+	// User management routes (requires admin role)
+	users := facility.Group("/users", m.RequireRole(types.UserRoleAdmin))
+	{
+		// Complete path: /app/:facility_code/users
+		users.GET("", h.HandleUsers)
+		users.POST("", h.HandleCreateUser)
+		// Complete path: /app/:facility_code/users/create
+		users.GET("/create", h.GetCreateUserForm)
+	}
 
-   // User routes (requires profile access)
-   user := facility.Group(PathUserInitials, m.RequireProfileAccess())
-   {
-       // Complete path: /app/:facility_code/:user_initials
-       user.GET("", h.HandleGetUser)
-       user.PUT("", h.HandleUpdateUser)
-       user.DELETE("", h.HandleDeleteUser, m.RequireRole(types.UserRoleAdmin))
-       // Complete path: /app/:facility_code/:user_initials/edit
-       user.GET("/edit", h.GetUpdateUserForm)
-       // Complete path: /app/:facility_code/:user_initials/password
-       user.GET("/password", h.GetUpdatePasswordForm)
-       user.PUT("/password", h.HandleUpdatePassword)
-       // Complete path: /app/:facility_code/:user_initials/availability/:id
-       user.POST("/availability/:id", h.HandleAvailabilityToggle)
-   }
+	// User routes (requires profile access)
+	user := facility.Group(PathUserInitials, m.RequireProfileAccess())
+	{
+		// Complete path: /app/:facility_code/:user_initials
+		user.GET("", h.HandleGetUser)
+		user.PUT("", h.HandleUpdateUser)
+		user.DELETE("", h.HandleDeleteUser, m.RequireRole(types.UserRoleAdmin))
+		// Complete path: /app/:facility_code/:user_initials/edit
+		user.GET("/edit", h.GetUpdateUserForm)
+		// Complete path: /app/:facility_code/:user_initials/password
+		user.GET("/password", h.GetUpdatePasswordForm)
+		user.PUT("/password", h.HandleUpdatePassword)
+		// Complete path: /app/:facility_code/:user_initials/availability/:id
+		user.POST("/availability/:id", h.HandleAvailabilityToggle)
+	}
 
-   // Schedule routes (require admin role)
-   schedule := user.Group("/schedule",  m.RequireRole(types.UserRoleAdmin))
-   {
-       // Complete path: /app/:facility_code/:user_initials/schedule
-       schedule.POST("", h.HandleCreateSchedule)
-       // Complete path: /app/:facility_code/:user_initials/schedule/create
-       schedule.GET("/create", h.GetCreateScheduleForm)
-       // Complete path: /app/:facility_code/:user_initials/schedule/:schedule_id
-       schedule.GET(PathScheduleID, h.HandleGetSchedule)
-       schedule.PUT(PathScheduleID, h.HandleUpdateSchedule)
-       // Complete path: /app/:facility_code/:user_initials/schedule/:schedule_id/edit
-       schedule.GET("/:schedule_id/edit", h.GetUpdateScheduleForm)
-   }
+	// Schedule routes (require admin role)
+	schedule := user.Group("/schedule", m.RequireRole(types.UserRoleAdmin))
+	{
+		// Complete path: /app/:facility_code/:user_initials/schedule
+		schedule.POST("", h.HandleCreateSchedule)
+		// Complete path: /app/:facility_code/:user_initials/schedule/create
+		schedule.GET("/create", h.GetCreateScheduleForm)
+		// Complete path: /app/:facility_code/:user_initials/schedule/:schedule_id
+		schedule.GET(PathScheduleID, h.HandleGetSchedule)
+		schedule.PUT(PathScheduleID, h.HandleUpdateSchedule)
+		// Complete path: /app/:facility_code/:user_initials/schedule/:schedule_id/edit
+		schedule.GET("/:schedule_id/edit", h.GetUpdateScheduleForm)
+	}
 }
